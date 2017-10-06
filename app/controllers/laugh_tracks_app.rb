f0dff7b1dcee7c513014257218801be87857426f
Emily Wise <wise.m.emily@gmail.com>
2017-10-06 10:49:37 -0600

    Select Age

--- app/controllers/laugh_tracks_app.rb
+++ app/controllers/laugh_tracks_app.rb
@@ -6,6 +6,20 @@ get '/comedians' do
   erb :index
 end
 
+get '/comedians' do
+  if params[:age]
+    @comedian = Comedian.where(age: params[:age])
+  else
+    @comedian = Comedian.all
+  end
+  erb :index
+end
+
+get '/comedians/:id' do
+  @comedian = Comedian.find(params[:id])
+  erb :show
+end
+
 
 
 end
